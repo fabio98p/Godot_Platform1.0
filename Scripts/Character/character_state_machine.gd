b3499f56316d9943f413f7cb57cf16954f65aee1
fabio98p <parisifabio98@gmail.com>
2024-09-03 22:56:23 +0200

    test state machine

---
+++ Scripts/Character/character_state_machine.gd
@@ -0,0 +1,41 @@
+extends Node
+
+class_name CharacterStateMachine
+
+@export var current_state : State
+@export var character: CharacterBody2D
+@onready var animation_tree: AnimationTree = $"../AnimationTree"
+
+
+var states: Array[State]
+
+func _ready() -> void:
+	for child in get_children():
+		if child is State:
+			states.append(child)
+			
+			child.character = character
+			child.playback = animation_tree["parameters/playback"]
+			
+		else:
+			push_warning("Child " + child.name + " is not a state for CharacterStateMachine")
+
+func _physics_process(delta: float) -> void:
+	if(current_state.next_state != null):
+		switch_state(current_state.next_state)
+	
+	current_state.state_process(delta)
+		
+func check_if_can_move() -> bool:
+	return current_state.can_move
+
+func switch_state(new_state: State):
+	if (current_state != null):
+		current_state.on_exit()
+		current_state.next_state = null
+		
+	current_state = new_state
+	current_state.on_enter()
+	
+func _input(event: InputEvent) -> void:
+	current_state.state_input(event)
